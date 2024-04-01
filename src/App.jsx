04a80c7705070828cb4228c386d42657a2ac823c
Januar Maksum <januarmaksum@gmail.com>
2024-04-01 08:34:24 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-[#F5D061]">Januar Maksum</h1>
+    </>
+  );
+}
+
+export default App;
