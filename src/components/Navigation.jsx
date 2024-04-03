7851ef35434a2ce2115124338214aef0e0d757e5
Januar Maksum <januarmaksum@gmail.com>
2024-04-03 07:47:11 +0700

    fix: bug class active navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,13 @@
 /* eslint-disable react/prop-types */
-export default function Navigation({ data, onPress }) {
+export default function Navigation({ data, onPress, spyId, classNameId }) {
+  const classActive = spyId === classNameId ? "active" : "";
+
   return (
-    <li>
+    <li data-to-scrollspy-id={data?.scrollspy_id}>
       <a
         onClick={(e) => onPress(e)}
         href={data?.href}
-        data-to-scrollspy-id={data?.scrollspy_id}
-        className="group flex items-center py-3"
+        className={`group flex items-center py-3 ${classActive}`}
       >
         <span className="nav-indicator mr-4 h-px w-8 bg-slate-400 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none"></span>
         <span className="nav-text text-xs font-bold uppercase tracking-widest text-slate-400 group-hover:text-slate-200 group-focus-visible:text-slate-200">
--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,8 +1,24 @@
 /* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
 import Avatar from "../assets/me.webp";
 import Navigation from "./Navigation";
 
 export default function Summary({ data, onPress }) {
+  const [classNameId, setClassNameId] = useState(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const hash = window.location.hash;
+      setClassNameId(hash);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div>
       <div className="relative w-[150px]">
@@ -14,7 +30,9 @@ export default function Summary({ data, onPress }) {
         />
         <div className="absolute border-primary rounded-lg z-10 top-2 -right-2 border w-[150px] h-[150px]" />
       </div>
-      <h1 className="text-3xl md:text-5xl font-bold text-primary mt-6">Januar Maksum</h1>
+      <h1 className="text-3xl md:text-5xl font-bold text-primary mt-6">
+        Januar Maksum
+      </h1>
       <h2 className="text-lg md:text-xl mt-0 md:mt-2 text-slate-200 font-semibold">
         Frontend Developer
       </h2>
@@ -24,7 +42,13 @@ export default function Summary({ data, onPress }) {
       <nav className="hidden lg:flex flex-col mt-12">
         <ul className="w-max">
           {data?.map((item) => (
-            <Navigation key={item?.nav_id} data={item} onPress={onPress} />
+            <Navigation
+              key={item?.nav_id}
+              data={item}
+              onPress={onPress}
+              spyId={item.href}
+              classNameId={classNameId}
+            />
           ))}
         </ul>
       </nav>
