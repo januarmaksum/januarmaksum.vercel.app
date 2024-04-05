bd8936d17b3e4e181d6d1e532bfeab112de85b66
Januar Maksum <januarmaksum@gmail.com>
2024-04-05 11:15:43 +0700

    feat: change content about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,31 +2,40 @@ export default function About() {
   return (
     <>
       <p>
-        Hello. 👋 I&apos;m Januar, a frontend developer based in Jakarta, who
-        enjoys building things that live on the internet. I develop exceptional
-        websites that provide intuitive, pixel-perfect user interfaces.
+        Hello👋 I&apos;m Januar. I started my career working in the B2B
+        marketplace in 2014 as a web designer and 1 year later switch career to
+        frontend developer. My interest in visuals and code made me learn a lot
+        about using supporting tools, such as software Visual Studio Code.
       </p>
       <p>
-        Shortly after graduating from{" "}
-        <span className="text-slate-200">
-          <a href="https://www.bsi.ac.id/" target="_blank">
-            BSI University
-          </a>
-        </span>{" "}
-        in 2014, I joined the B2B marketplace company at{" "}
-        <span className="text-slate-200">
-          <a href="https://www.indotrading.com/" target="_blank">
-            Indotrading
-          </a>
-        </span>{" "}
-        where I work on a wide variety of interesting and meaningful projects on
-        a daily basis.
+        Equipped with knowledge from studying informatics engineering, I
+        understand how systems work behind the scenes until they are presented
+        to users. Understand the basic framework of programming languages, for
+        example HTML, CSS and JavaScript.
       </p>
       <p>
-        Experienced frontend developer more than 9 years in building modern and
-        user-friendly web applications. Proficient in web development
-        technologies, with a strong focus on performance, stability and
-        maintainability.
+        As a frontend developer, I have more than 9 years of experience in
+        building modern and user-friendly web applications. Proficient in web
+        development technologies, with a strong focus on performance, stability
+        and maintainability.
+      </p>
+      <p>
+        Currently I live in Jakarta, Indonesia. You can contact me at{" "}
+        <a
+          href="mailto:januarmaksum@gmail.com"
+          className="text-slate-200 underline"
+          target="_blank"
+        >
+          januarmaksum@gmail.com
+        </a>{" "}
+        or connect on{" "}
+        <a
+          href="https://www.linkedin.com/in/januarmaksum/"
+          target="_blank"
+          className="text-slate-200 underline"
+        >
+          Linkedin
+        </a>
       </p>
     </>
   );
