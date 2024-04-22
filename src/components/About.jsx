8c4a0cc6797ce7a7e4174ded5beb030085aeb5b3
Januar Maksum <januarmaksum@gmail.com>
2024-04-22 07:23:03 +0700

    update about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,11 +3,10 @@ export default function About() {
     <>
       <p>
         👋 Hi I&apos;m Januar. A Frontend Developer based in Jakarta,
-        Indonesia. 📍
+        Indonesia. Specializing in React, Next.js, TailwindCSS, and React Native.
       </p>
-      <p>🚀 Specializing in React, Next.js, TailwindCSS, and React Native.</p>
-      <p>👨‍💻 My passion is to create and develop a clean UI/UX. Additionally, my proficiency extends to React Native, enabling me to develop cross-platform mobile applications efficiently.</p>
-      <p>🧰 Here are a few technologies I&apos;ve been working with recently:</p>
+      <p>My passion is to create and develop a clean UI/UX. Additionally, my proficiency extends to React Native, enabling me to develop cross-platform mobile applications efficiently.</p>
+      <p>Here are a few technologies I&apos;ve been working with recently:</p>
       <ul className="columns-3 xl:columns-4 list-disc pl-4 text-sm grid-flow-col">
         <li>HTML</li>
         <li>CSS</li>
@@ -22,7 +21,7 @@ export default function About() {
         <li>SASS</li>
         <li>Wordpress</li>
       </ul>
-      <p>💼 Currently seeking opportunities to collaborate on innovative projects and contribute to dynamic development teams. Let's connect and explore how we can create impactful solutions together!</p>
+      <p>Currently seeking opportunities to collaborate on innovative projects and contribute to dynamic development teams. Let&apos;s connect and explore how we can create impactful solutions together!</p>
     </>
   );
 }
