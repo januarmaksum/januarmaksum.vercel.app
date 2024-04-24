c8f047c04bedc4f089a3a2e5f2cc714a7a27deaa
Januar Maksum <januarmaksum@gmail.com>
2024-04-24 07:52:56 +0700

    fix: ui break in lower screen

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -20,7 +20,7 @@ export default function Summary({ data, onPress }) {
 
   return (
     <div>
-      <div className="relative w-[150px] group">
+      <div className="relative w-[150px] group avatar">
         <img
           src={Avatar}
           alt="januar maksum"
@@ -30,13 +30,13 @@ export default function Summary({ data, onPress }) {
         />
         <div className="absolute group-hover:bg-primary transition-all group-hover:!bg-none border-primary rounded-lg z-10 top-2 -right-2 border w-[150px] h-[150px]" style={{ backgroundImage: "url(/frame_bg.svg)" }} />
       </div>
-      <h1 className="text-3xl md:text-5xl font-bold text-primary mt-6">
+      <h1 className="text-3xl md:text-5xl font-bold text-primary mt-6 title-page">
         Januar Maksum
       </h1>
       <h2 className="text-lg md:text-xl mt-0 md:mt-2 text-slate-200 font-semibold">
         Frontend Developer
       </h2>
-      <nav className="hidden lg:flex flex-col mt-12">
+      <nav className="hidden lg:flex flex-col mt-12 mb-12">
         <ul className="w-max">
           {data?.map((item) => (
             <Navigation
